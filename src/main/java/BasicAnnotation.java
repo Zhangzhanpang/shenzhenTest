0185e3245c5c4a10fff36059c21dc7184996ddb5
zhangzhanpeng <527048188@qq.com>
2019-10-01 18:23:54 +0800

    可以使用的初级版本

---
+++ src/main/java/BasicAnnotation.java
@@ -0,0 +1,6 @@
+public class BasicAnnotation {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/BasicAnnotationTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class BasicAnnotationTest {
+
+    @Test
+    public void testMain() {
+        System.out.println("哈哈哈");
+    }
+
+
+}
